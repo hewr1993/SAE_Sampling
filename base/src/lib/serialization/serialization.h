562ad485de1a0be58bd04bd9cac62f11e1be2517
hewr1993 <hewr2010@gmail.com>
2015-04-17 16:59:07 +0800

    runnable startup storage (from saedb)

---
+++ base/src/lib/serialization/serialization.h
@@ -0,0 +1,11 @@
+#include "iserializestream.h"
+#include "oserializestream.h"
+#include "basic_types.h"
+
+#include "string_convert.h"
+
+#include "list.h"
+#include "vector.h"
+#include "map.h"
+#include "set.h"
+#include "iterator.h"
---
+++ base/src/main.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> Author: Wayne Ho
+	> Purpose: TODO
+	> Created Time: Fri Apr 17 16:32:22 2015
+	> Mail: hewr2010@gmail.com 
+ ************************************************************************/
+#include "graph_builder.h"
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
+
