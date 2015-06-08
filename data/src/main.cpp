50de728a2310c51d56c4883f207bdd37de2eefe1
SherlockYang <SherlockBourne@gmail.com>
2015-06-08 15:59:10 +0800

    update

--- data/src/main.cpp
+++ data/src/main.cpp
@@ -8,24 +8,41 @@
 #include <iostream>
 #include <cstdio>
 #include <fstream>
+#include <map>
 using namespace std;
 using namespace sae::io;
+    
+map<string, int> nodeMap;
+
+int GetOrInsert(const string& key)
+{
+    map<string, int>::iterator it = nodeMap.find(key);
+    if (it != nodeMap.end())
+        return it -> second;
+    int id = (int) nodeMap.size();
+    nodeMap.insert(make_pair(key, id));
+    return id;
+}
 
 int main(int argc, char **argv) {
     GraphBuilder<int> graph;
-    ifstream fin("./resource/amazon0312.txt");
+    //ifstream fin("./resource/amazon0312.txt");
+    ifstream fin("./resource/twitter_combined.txt");
     string buf;
-    for (int i = 0; i < 4; ++i) getline(fin, buf);
+    //for (int i = 0; i < 4; ++i) getline(fin, buf);
     int v_cnt(-1);
+    map<string, int> nodeMap;
     while (1) {
-        int x, y;
+        string x, y;
         if (!(fin >> x >> y)) break;
-        int z = max(max(v_cnt, x), y);
-        while (v_cnt < z) graph.AddVertex(++v_cnt, 0);
-        graph.AddEdge(x, y, 0);
+        int a = GetOrInsert(x);
+        int b = GetOrInsert(y);
+        int c = max(max(v_cnt, a), b);
+        while (v_cnt < c) graph.AddVertex(++v_cnt, 0);
+        graph.AddEdge(a, b, 0);
     }
     cout << graph.VertexCount() << " " << graph.EdgeCount() << endl;
-    graph.Save("./data/graph");
+    graph.Save("./data/twitter");
     return 0;
 }
 
